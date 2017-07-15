b51c39aed09711e0cd1ca671a829ec1ad4b16ec8
emersonrjr03 <emersonrjr03@301522c3-9a36-429e-a30d-fde608505948>
2017-07-15 04:23:08 +0000

    Commitando projeto em JavaFX

--- trunk/CarRent/src/carrent/CarRent.java
+++ trunk/CarRent/src/carrent/CarRent.java
@@ -5,17 +5,47 @@
  */
 package carrent;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  *
- * @author MarcoTulio
+ * @author Emerson
  */
-public class CarRent {
+public class CarRent extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
