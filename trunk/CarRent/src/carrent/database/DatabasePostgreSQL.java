f8f29c88c751900e13770c3bb2f42f6e9dfb27de
emersonrjr03 <emersonrjr03@301522c3-9a36-429e-a30d-fde608505948>
2017-07-17 23:30:44 +0000

    comentando a merda que eu fiz na classe de conecção;

--- trunk/CarRent/src/carrent/database/DatabasePostgreSQL.java
+++ trunk/CarRent/src/carrent/database/DatabasePostgreSQL.java
@@ -6,12 +6,15 @@ import java.sql.DriverManager;
 
 
 public class DatabasePostgreSQL {
-    private Connection connection;
-    @Override
-    public Connection conectar(){
-        try{
-            Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/carrent","postgres","postgres");
-        }
-    }
+//    private Connection connection;;;
+//    @Override
+//    public Connection conectar(){
+//        try{
+//            Class.forName("org.postgresql.Driver");
+//            this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1/carrent","postgres","postgres");
+//        }
+//        catch(Exception e){
+//        
+//        }
+//    };
 }
