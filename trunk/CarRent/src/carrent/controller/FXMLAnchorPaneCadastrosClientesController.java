e5fd9b00a2ece1586acad3dfecf99ccff3ad2d0d
emersonrjr03 <emersonrjr03@301522c3-9a36-429e-a30d-fde608505948>
2017-07-21 04:23:29 +0000

    Agora da tela principal é possivel ir até a tela de cadastro de clientes. O cliente agoar tem todas as funçoes de insert,update,delete,select e tambem tem uma função que retorna uma lista de clientes, para preencher a tabela na tela de cadastro de clientes.

--- trunk/CarRent/src/carrent/controller/FXMLAnchorPaneCadastrosClientesController.java
+++ trunk/CarRent/src/carrent/controller/FXMLAnchorPaneCadastrosClientesController.java
@@ -13,9 +13,11 @@ import carrent.DAO.*;
 import carrent.database.ConnectionFactory;
 import java.sql.Connection;
 import java.util.List;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLAnchorPaneCadastrosClientesController implements Initializable {
 
@@ -46,12 +48,23 @@ public class FXMLAnchorPaneCadastrosClientesController implements Initializable 
     private ObservableList<Cliente> observableListClientes;
     
     private final Connection connection = new ConnectionFactory().getConnection();
-    private final Cliente clienteDAO = new Cliente();
+    private final Cliente Cliente = new Cliente();
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Cliente.setConnection(connection);
+        carregarTableViewClientes();
+        
+    }   
+    public void carregarTableViewClientes(){
+        tablecolumnClienteNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        tablecolumnClienteCPF.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+        
+        listClientes = Cliente.listar();
+        
+        observableListClientes = FXCollections.observableList(listClientes);
+        tableViewClientes.setItems(observableListClientes);
+    }
     
 }
